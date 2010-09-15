f297ef4f77454dbe46a04c1b67a0ecd0be1f2c54
Marak Squires <marak.squires@gmail.com>
2010-09-15 05:31:54 -0400

    [api] continued to stub out structure

---
+++ app/monitor/index.js
@@ -0,0 +1,15 @@
+// simple monitoring application 
+
+
+var sys = require('sys'), eyes = require('eyes');
+var hookio = require('../../lib/hookio');
+
+
+// we are going to hardcode a hook here to demonstrate when a hook might look like
+
+eyes.inspect(hookio);
+
+exports = function() {
+  
+  return hook;
+};
