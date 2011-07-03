197d05a9895becf5ac97a12696d5dc9ff4b709da
Marak Squires <marak.squires@gmail.com>
2011-07-03 00:40:58 -0700

    [fix]: Siblings tests now respect `ready`, `listening`, and `connected` events accordingly.

--- tests/siblings/test-sibling-callresponse.js
+++ tests/siblings/test-sibling-callresponse.js
@@ -35,69 +35,68 @@ function fired( event, data ){
   expected_events = helpers.fired(event, expected_events, data);
 };
 
-
-
-
 // Test the ability to start up an output hook that other hooks can connect to
 // this output will receive and rebroadcast messages to all its children ( which are siblings to each other )
 var Hook = require('hook.io').Hook;
 var hook_simple_output = new Hook({ name: 'simple-output' });
 var colors = require('colors');
 hook_simple_output.listen({ port: 5001 });
-fired('simple-output started');
 
 
-hook_simple_output.on('ready', function(){
+hook_simple_output.on('listening', function(){
 
+  fired('simple-output started');
   // Tests the ability to do a call and response to a sibling
   // this input will wait for a sibling to send it a message and then respond back
 
   var simple_input_callrsp = new Hook({ name: 'simple-input-responder' });
   simple_input_callrsp.connect({ port: 5001 });
-  fired( 'simple-input-responder connected')
 
-  simple_input_callrsp.on('i.getSomething.o.getSomething', function(source, event, data) {
 
-    //
-    // perfom some logic here
-    //
-    fired( 'simple-input-responder-receive ' + source, data)
-    simple_input_callrsp.emit('o.gotResponse', 'foobar');
+  simple_input_callrsp.on('connected', function(){
+
+    fired( 'simple-input-responder connected');
+
+    simple_input_callrsp.on('i.getSomething.o.getSomething', function(source, event, data) {
+
+      //
+      // perfom some logic here
+      //
+      fired( 'simple-input-responder-receive ' + source, data)
+      simple_input_callrsp.emit('o.gotResponse', 'foobar');
+
+    });
+    
+    fired( 'simple-input-responder-listen i.getSomething')
+
+    // Tests the ability to do a call and response to a sibling
+    // this input will wait for a sibling to send it a message and then respond back
+
+    var simple_input_callrsp2 = new Hook({ name: 'simple-input-caller' });
+    simple_input_callrsp2.connect({ port: 5001 });
+
+    simple_input_callrsp2.on('i.gotResponse.o.gotResponse', function(source, event, data) {
+      fired('simple-input-caller-receive ' + source, data);
+    });
+    fired( 'simple-input-caller-listen i.gotResponse')
+
+    simple_input_callrsp2.on('connected', function(){
+
+      fired('simple-input-caller connected');
+
+      simple_input_callrsp2.emit('o.getSomething', 'i need a value please');
+      fired('simple-input-caller-call o.getSomething', 'i need a value please');
+    });
+
+    // Perform a cleanup and calculate totals after a little while
+    setTimeout(function(){
+
+      helpers.report('test-sibling-callresponse.js', expected_events);
+
+    }, 2000);
 
   });
-  fired( 'simple-input-responder-listen i.getSomething')
 
-
-
-  // Tests the ability to do a call and response to a sibling
-  // this input will wait for a sibling to send it a message and then respond back
-
-  var simple_input_callrsp2 = new Hook({ name: 'simple-input-caller' });
-  simple_input_callrsp2.connect({ port: 5001 });
-  fired('simple-input-caller connected');
-
-
-  simple_input_callrsp2.on('i.gotResponse.o.gotResponse', function(source, event, data) {
-    fired('simple-input-caller-receive ' + source, data);
-  });
-  fired( 'simple-input-caller-listen i.gotResponse')
-
-
-  simple_input_callrsp2.on('ready', function(){
-    simple_input_callrsp2.emit('o.getSomething', 'i need a value please');
-    fired('simple-input-caller-call o.getSomething', 'i need a value please');
-  });
-
-
-
-  // Perform a cleanup and calculate totals after a little while
-  setTimeout(function(){
-
-    helpers.report('test-sibling-callresponse.js', expected_events);
-
-  }, 2000);
-  
-  
 });
 
 
--- tests/siblings/test-sibling-message.js
+++ tests/siblings/test-sibling-message.js
@@ -31,49 +31,55 @@
   var hook_simple_output = new Hook({ name: 'simple-output' });
   var colors = require('colors');
   hook_simple_output.listen();
-  fired('simple-output started');
 
-
-  hook_simple_output.on('ready', function(){
+  hook_simple_output.on('listening', function(){
     
+    fired('simple-output started');
 
     // Tests the ability to start up a hook input listen for simple messages from siblings 
     // this input will wait for messages from its siblings
     var hook_simple_message_subscriber = new Hook({ name: 'simple-input-handler' });
     hook_simple_message_subscriber.connect();
-    fired('simple-input connected');
 
-    hook_simple_message_subscriber.on('i.test.o.test', function(source, event, data) {
-      var expect = 'hello there!';
+    hook_simple_message_subscriber.on('connected', function(){
 
-      if(data !== expect){
-        //h(false, expect, data);
-      }
-      else {
-        fired('simple-input-handler-receive ' + source, data);
-      }
+      fired('simple-input connected');
+      
+      hook_simple_message_subscriber.on('i.test.o.test', function(source, event, data) {
+        var expect = 'hello there!';
 
+        if(data !== expect){
+          //h(false, expect, data);
+        }
+        else {
+          fired('simple-input-handler-receive ' + source, data);
+        }
+
+      });
+      fired('simple-input-handler-listen i.test.o.test');
+
+      // Tests the ability to start up a hook input and broadcast a simple message to it's siblings 
+      // this input will broadcast a message on ready
+      var hook_simple_message = new Hook({ name: 'simple-input-caller' });
+      hook_simple_message.connect();
+
+      hook_simple_message.on('connected', function(){
+        fired('simple-input-caller connected');
+        hook_simple_message.emit('o.test', 'hello there!');
+        fired('simple-input-caller-call o.test', 'hello there!')
+      });
+
+      // Perform a cleanup and calculate totals after a little while
+      setTimeout(function(){
+
+        helpers.report('test-sibling-message.js', expected_events);
+
+      }, 2000);
+      
+      
     });
-    fired('simple-input-handler-listen i.test.o.test');
 
-    // Tests the ability to start up a hook input and broadcast a simple message to it's siblings 
-    // this input will broadcast a message on ready
-    var hook_simple_message = new Hook({ name: 'simple-input-caller' });
-    hook_simple_message.connect();
-    fired('simple-input-caller connected');
-    hook_simple_message.on('ready', function(){
-      hook_simple_message.emit('o.test', 'hello there!');
-      fired('simple-input-caller-call o.test', 'hello there!')
-    });
 
-    // Perform a cleanup and calculate totals after a little while
-    setTimeout(function(){
-
-      helpers.report('test-sibling-message.js', expected_events);
-
-    }, 2000);
-
-    
   });
 
 
