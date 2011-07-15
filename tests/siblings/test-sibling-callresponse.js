e45c436b681215b601167f18d2253a69ad00bd6f
Marak Squires <marak.squires@gmail.com>
2011-07-15 00:19:09 -0700

    [test]: Continued rename of output=>server and input=>client

--- tests/siblings/test-sibling-callresponse.js
+++ tests/siblings/test-sibling-callresponse.js
@@ -2,28 +2,28 @@ var helpers = require('../helpers');
 
 
 var expected_events = {
-  "simple-output started" : {
+  "simple-server started" : {
     expected: 1
   },
-  "simple-input-responder connected" : {
+  "simple-client-responder connected" : {
     expected: 1
   },
-  "simple-input-responder-listen *.getSomething" : {
+  "simple-client-responder-listen *.getSomething" : {
     expected: 1
   },
-  "simple-input-caller connected" : {
+  "simple-client-caller connected" : {
     expected: 1
   },
-  "simple-input-caller-listen *.gotResponse" : {
+  "simple-client-caller-listen *.gotResponse" : {
     expected: 1
   },
-  "simple-input-caller-call *.getSomething" : {
+  "simple-client-caller-call *.getSomething" : {
     expected: 1
   },
-  "simple-input-responder-receive simple-output.getSomething" : {
+  "simple-client-responder-receive simple-server.getSomething" : {
     expected: 1
   },
-  "simple-input-caller-receive simple-output.gotResponse" : {
+  "simple-client-caller-receive simple-server.gotResponse" : {
     expected: 1
   }
   
@@ -38,54 +38,54 @@ function fired( event, data ){
 // Test the ability to start up an output hook that other hooks can connect to
 // this output will receive and rebroadcast messages to all its children ( which are siblings to each other )
 var Hook = require('hook.io').Hook;
-var hook_simple_output = new Hook({ name: 'simple-output' });
+var hook_simple_server = new Hook({ name: 'simple-server' });
 var colors = require('colors');
-hook_simple_output.listen({ port: 5001 });
+hook_simple_server.listen({ port: 5001 });
 
 
-hook_simple_output.on('listening', function(){
+hook_simple_server.on('listening', function(){
 
-  fired('simple-output started');
+  fired('simple-server started');
   // Tests the ability to do a call and response to a sibling
   // this input will wait for a sibling to send it a message and then respond back
 
-  var simple_input_callrsp = new Hook({ name: 'simple-input-responder' });
-  simple_input_callrsp.connect({ port: 5001 });
+  var simple_client_callrsp = new Hook({ name: 'simple-client-responder' });
+  simple_client_callrsp.connect({ port: 5001 });
 
 
-  simple_input_callrsp.on('connected', function(){
+  simple_client_callrsp.on('connected', function(){
 
-    fired( 'simple-input-responder connected');
+    fired( 'simple-client-responder connected');
 
-    simple_input_callrsp.on('*.getSomething', function(source, event, data) {
+    simple_client_callrsp.on('*.getSomething', function(source, event, data) {
 
       //
       // perfom some logic here
       //
-      fired( 'simple-input-responder-receive ' + source, data)
-      simple_input_callrsp.emit('*.gotResponse', 'foobar');
+      fired( 'simple-client-responder-receive ' + source, data)
+      simple_client_callrsp.emit('*.gotResponse', 'foobar');
 
     });
     
-    fired( 'simple-input-responder-listen *.getSomething')
+    fired( 'simple-client-responder-listen *.getSomething')
 
     // Tests the ability to do a call and response to a sibling
     // this input will wait for a sibling to send it a message and then respond back
 
-    var simple_input_callrsp2 = new Hook({ name: 'simple-input-caller' });
-    simple_input_callrsp2.connect({ port: 5001 });
+    var simple_client_callrsp2 = new Hook({ name: 'simple-client-caller' });
+    simple_client_callrsp2.connect({ port: 5001 });
 
-    simple_input_callrsp2.on('*.gotResponse', function(source, event, data) {
-      fired('simple-input-caller-receive ' + source, data);
+    simple_client_callrsp2.on('*.gotResponse', function(source, event, data) {
+      fired('simple-client-caller-receive ' + source, data);
     });
-    fired( 'simple-input-caller-listen *.gotResponse')
+    fired( 'simple-client-caller-listen *.gotResponse')
 
-    simple_input_callrsp2.on('connected', function(){
+    simple_client_callrsp2.on('connected', function(){
 
-      fired('simple-input-caller connected');
+      fired('simple-client-caller connected');
 
-      simple_input_callrsp2.emit('*.getSomething', 'i need a value please');
-      fired('simple-input-caller-call *.getSomething', 'i need a value please');
+      simple_client_callrsp2.emit('*.getSomething', 'i need a value please');
+      fired('simple-client-caller-call *.getSomething', 'i need a value please');
     });
 
     // Perform a cleanup and calculate totals after a little while
--- tests/siblings/test-sibling-message.js
+++ tests/siblings/test-sibling-message.js
@@ -1,22 +1,22 @@
   var helpers = require('../helpers');
 
   var expected_events = {
-    "simple-output started" : {
+    "simple-server started" : {
       expected: 1
     },
-    "simple-input connected" : {
+    "simple-client connected" : {
       expected: 1
     },
-    "simple-input-handler-listen *.test" : {
+    "simple-client-handler-listen *.test" : {
       expected: 1
     },
-    "simple-input-caller connected" : {
+    "simple-client-caller connected" : {
       expected: 1
     },
-    "simple-input-caller-call test" : {
+    "simple-client-caller-call test" : {
       expected: 1
     },
-    "simple-input-handler-receive simple-output.test" : {
+    "simple-client-handler-receive simple-server.test" : {
       expected: 1
     }
   };
@@ -28,22 +28,22 @@
   // Test the ability to start up an output hook that other hooks can connect to
   // this output will receive and rebroadcast messages to all its children ( which are siblings to each other )
   var Hook = require('hook.io').Hook;
-  var hook_simple_output = new Hook({ name: 'simple-output' });
+  var hook_simple_server = new Hook({ name: 'simple-server' });
   var colors = require('colors');
-  hook_simple_output.listen();
+  hook_simple_server.listen();
 
-  hook_simple_output.on('listening', function(){
+  hook_simple_server.on('listening', function(){
     
-    fired('simple-output started');
+    fired('simple-server started');
 
     // Tests the ability to start up a hook input listen for simple messages from siblings 
     // this input will wait for messages from its siblings
-    var hook_simple_message_subscriber = new Hook({ name: 'simple-input-handler' });
+    var hook_simple_message_subscriber = new Hook({ name: 'simple-client-handler' });
     hook_simple_message_subscriber.connect();
 
     hook_simple_message_subscriber.on('connected', function(){
 
-      fired('simple-input connected');
+      fired('simple-client connected');
       
       hook_simple_message_subscriber.on('*.test', function(source, event, data) {
         var expect = 'hello there!';
@@ -52,21 +52,21 @@
           //h(false, expect, data);
         }
         else {
-          fired('simple-input-handler-receive ' + source, data);
+          fired('simple-client-handler-receive ' + source, data);
         }
 
       });
-      fired('simple-input-handler-listen *.test');
+      fired('simple-client-handler-listen *.test');
 
       // Tests the ability to start up a hook input and broadcast a simple message to it's siblings 
       // this input will broadcast a message on ready
-      var hook_simple_message = new Hook({ name: 'simple-input-caller' });
+      var hook_simple_message = new Hook({ name: 'simple-client-caller' });
       hook_simple_message.connect();
 
       hook_simple_message.on('connected', function(){
-        fired('simple-input-caller connected');
+        fired('simple-client-caller connected');
         hook_simple_message.emit('*.test', 'hello there!');
-        fired('simple-input-caller-call test', 'hello there!')
+        fired('simple-client-caller-call test', 'hello there!')
       });
 
       // Perform a cleanup and calculate totals after a little while
