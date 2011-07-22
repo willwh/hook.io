a8c7f5e19fec8985c747879c711547d1ab4056a0
Jameson Lee <jamesonjlee@gmail.com>
2011-07-21 20:09:23 -0400

    [test] filled out spawn tests [lib]  debugged .spawn [misc] removed dev-dep on old plugins

--- test/spawn/basic-spawn-forever-test.js
+++ test/spawn/basic-spawn-forever-test.js
@@ -9,8 +9,38 @@
 var vows = require('vows'),
     assert = require('assert'),
     Hook = require('../../lib/hookio').Hook,
+    forver = require('forever'),
     macros = require('../helpers/macros');
 
-vows.describe('hook.io/discovery/basic-spawn').addBatch({
+var PORT = 5021;
+
+vows.describe('hook.io/spawn/basic-forever-spawn').addBatch({
   
+  "When a hook is listening on a PORT": macros.assertListen('simple-host', PORT, {
+    "and we ask it to spawn some children" : {
+      topic : function (host) {
+        host.spawn('simple', this.callback.bind(this));
+        //host.spawn(['simple'], this.callback.bind(this));
+        //host.spawn(['simple', 'troll'], this.callback.bind(this));
+      },
+      "it should have spawned children" : function (_, host) {
+        assert.isObject(host.children);
+        assert.notEqual(Object.keys(host.children).length, 0);
+      },
+      "that is called simple, with a known bin" : function (_, host) {
+        assert.isObject(host.children);
+        assert.isObject(host.children['hook.io-' +'simple']);
+        assert.isString(host.children['hook.io-' +'simple'].bin);
+      },
+      "not in local mode" : function (_, host) {
+        assert.isUndefined(host.local);
+      },
+      "without coughing up an error" : function (err, host) {
+        assert.notEqual(typeof err, 'Error');
+        assert.isNull(err);
+      }
+    }
+  }),
+}).addBatch({
+
 }).export(module);
--- test/spawn/basic-spawn-test.js
+++ test/spawn/basic-spawn-test.js
@@ -11,6 +11,33 @@ var vows = require('vows'),
     Hook = require('../../lib/hookio').Hook,
     macros = require('../helpers/macros');
 
+var PORT = 5020;
+
 vows.describe('hook.io/discovery/basic-spawn').addBatch({
-  
+  "When a hook is listening on a PORT": macros.assertReady('simple-host', PORT, {
+    "and we ask it to be local and begin spawning" : {
+      topic : function (host) {
+        host.local = true;
+        host.spawn('simple', this.callback.bind(this));
+        //host.spawn(['simple'], this.callback.bind(this));
+        //host.spawn(['simple', 'troll'], this.callback.bind(this));
+      },
+      "it should have spawned children" : function (_, host) {
+        assert.isObject(host.children);
+      },
+      "that is called simple" : function (_, host) {
+        assert.isObject(host.children);
+        assert.isObject(host.children['hook.io-' +'simple']);
+      },
+      "in local mode" : function (_, host) {
+        assert.isTrue(host.local);
+      },
+      "without coughing up an error" : function (err, host) {
+        assert.notEqual(typeof err, 'Error');
+        assert.isNull(err);
+      }
+    }
+  }),
+}).addBatch({
+
 }).export(module);
