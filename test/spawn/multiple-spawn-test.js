95e85701cf984fc4fae7cf658a155b9e9619745a
indexzero <charlie.robbins@gmail.com>
2011-07-22 17:03:58 -0700

    [test] Add multiple-spawn-test.js stub

---
+++ test/spawn/multiple-spawn-test.js
@@ -0,0 +1,38 @@
+/*
+ * test-basic-test.js: Basic tests for the hook.io module
+ *
+ * (C) 2011 Marak Squires, Charlie Robbins
+ * MIT LICENCE
+ *
+ */
+
+var vows = require('vows'),
+    assert = require('assert'),
+    Hook = require('../../lib/hookio').Hook,
+    macros = require('../helpers/macros');
+
+vows.describe('hook.io/spawn/multiple-spawn').addBatch({
+}).export(module);
+
+//
+// Remark: This is not currently possible in hook.io, 
+// but it should be!!
+//
+// var hook = new hookio.Hook();
+// 
+// hook.listen();
+// 
+// hook.on('hook::listening', function () {
+//   hook.spawn([
+//     {
+//       type: 'webserver',
+//       name: 'webserver-0'
+//       options: { port: 9001 } 
+//     },
+//     {
+//       type: 'webserver',
+//       name: 'webserver-1'
+//       options: { port: 9002 } 
+//     }
+//   ])
+// });
