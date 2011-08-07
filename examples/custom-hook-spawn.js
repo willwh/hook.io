d42524f0261c9c9ea02aeb28319f76c793b4bfef
Marak Squires <marak.squires@gmail.com>
2011-08-07 05:12:51 -0700

    [minor]: Updates for /examples/ [fix]: Forget to switch console padding back to default

--- examples/custom-hook-spawn.js
+++ examples/custom-hook-spawn.js
@@ -2,30 +2,7 @@
  * Custom Hook-io Spawn
  */
 
-var Hook = require('../lib/hookio').Hook,
-    util = require('util');
-
-
-var Helloworld = exports.Helloworld = function (options) {
-
-  var self = this;
-  Hook.call(this, options);
-  
-  this.on('hook::ready', function () {
-    //
-    // This is just to simulate I/O, don't use timers...
-    //
-    setInterval(function () {
-      self.emit('hello', 'Hello, I am ' + self.name);
-    }, 1000);
-  });
-};
-
-//
-// Inherit from `hookio.Hook`
-//
-util.inherits(Helloworld, Hook);
-
+var Helloworld = require('hook.io-helloworld').Helloworld;
 
 
 var myHello = new Helloworld({name:"a"});
