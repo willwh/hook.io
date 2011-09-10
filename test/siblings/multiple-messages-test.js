e0c295cbfb9539142cc01c779db82a2271101892
Mickael Daniel <daniel.mickael@gmail.com>
2011-09-10 18:09:01 +0200

    refactor to use hasEvent instead of getEvent, handle wildcards and edit the test to easily change the number of subscriber

--- test/siblings/multiple-messages-test.js
+++ test/siblings/multiple-messages-test.js
@@ -2,8 +2,8 @@
  * multiple-messages-test.js: Test the ability to work with multiple hook and rather important message passing
  *                            to demonstrate some heavier network usage.
  *
- *                            This test suite init and start a server hook with a predefined number 
- *                            of clients.
+ *                            This test suite init and start a server hook with a predefined number
+ *                            of clients, emiting back and fourth the content of browser/hook.js (150kb).
  *
  * (C) 2011 Marak Squires, Charlie Robbins
  * MIT LICENCE
@@ -18,104 +18,71 @@ var vows = require('vows'),
     macros = require('../helpers/macros'),
     EventEmitter = require('events').EventEmitter;
 
-// create a local macro
-// TODO: see if it's better to put this in helpers/macros
 
+// The local fixture, map to browser/hook.js
 var fixture = fs.readFileSync(path.join(__dirname, '../../browser/hook.js')).toString();
 
+// create a local macro
+// TODO: see if it's better to put this in helpers/macros
 var macro = function(event, port){
-  
+
   var context = {
-    topic: function(subscriber, data) {
+    topic: function(server, data) {
       var messager = new Hook({ name: 'simple-client-messager' });
 
-      subscriber.once('*::' + event, function(data) {
-        subscriber.emit('response.' + event, data);
+      server.once('*::' + event, function(data) {
+        server.emit('response.' + event, data);
       });
 
-      messager.once('*::response.' + event, this.callback.bind(subscriber, null));
+      messager.once('*::response.' + event, this.callback.bind(server, null));
 
       messager.connect({ 'hook-port': port });
       messager.once('hook::connected', function () {
-        messager.emit(event, fixture);
+        messager.emit(event, {content: fixture});
       });
     }
   };
 
   context["the *::" + event + " should be fired correctly"] = function(data) {
-    assert.equal(data, fixture);
+    assert.equal(data.content, fixture);
   };
 
   return context;
 };
 
+// creates a context with a preder 
+macro.multipleSubscriber = function(prefix, count) {
+  var context = {},
+      test = count;
 
+  context["and another hook connects to listen *::test::**"] = {
+    topic: function() {
+      var listener = new Hook({ name: 'simple-listener' }),
+          self = this,
+          length = count - 1;
+
+      listener.connect({ 'hook-port': 5002 });
+      listener.on('*::test::*::*', function log () {
+        if(--length === 0) self.callback();
+        console.log('    ', this.event.red.bold, length);
+      });
+    },
+
+    "should be cool": function() {
+      assert.ok(true);
+    }
+  };
+
+  while(--test) {
+    context["and another hook connects to emit *::" + prefix + test] = macro(prefix + test, 5002);
+  }
+
+  return context;
+};
+
+
+// Start the batch with a with a predefined number of cycles
 vows.describe('hook.io/siblings/multiple-message').addBatch({
-  "When a hook is listening on 5050": macros.assertListen('simple-server', 5002, {
-    "and another hook connects to emit *::test": macro('test', 5002),
-    "and another hook connects to emit *::test::one": macro('test::one', 5002),
-    "and another hook connects to emit *::test::two": macro('test::two', 5002),
-    "and another hook connects to emit *::test::three": macro('test::three', 5002),
-    "and another hook connects to emit *::test::four": macro('test::four', 5002),
-    "and another hook connects to emit *::test::five": macro('test::five', 5002),
-    "and another hook connects to emit *::test::six": macro('test::six', 5002)
-  })
+  "When a hook is listening on 5050": macros.assertListen('simple-server', 5002, macro.multipleSubscriber('test::foo::', 12))
 }).export(module);
 
-
-/*
-vows.describe('hook.io/siblings/message').addBatch({
-  "When a hook is listening on 5050": macros.assertListen('simple-server', 5002, {
-    "and another hook connects": macros.assertConnect('simple-subscriber', 5002, {
-      "and emits *::test": {
-        topic: function (subscriber) {
-          var messager = new Hook({ name: 'simple-client-messager' });
-
-          subscriber.on('*::test', this.callback.bind(subscriber, null, subscriber));
-
-          messager.connect({ "hook-port": 5002 });
-          messager.once('hook::connected', function () {
-            messager.emit('test', fixture);
-          });
-
-        },
-
-        "the *::test event should be fired correctly": {
-          topic: function(subscriber, data) {
-            var messager = new Hook({ name: 'simple-client-messager' });
-
-            subscriber.on('*::test1', this.callback.bind(subscriber, null));
-
-            messager.connect({ "hook-port": 5002 });
-            messager.once('hook::connected', function() {
-              messager.emit('test1', fixture);
-            });
-          },
-
-          "data should be valid buddy": function(data) {
-            assert.equal(data, fixture);
-          }
-        },
-
-
-        "the *::test2 event should be fired correctly": {
-          topic: function(subscriber, data) {
-            var messager = new Hook({ name: 'simple-client-messager' });
-
-            subscriber.on('*::test2', this.callback.bind(subscriber, null));
-
-            messager.connect({ "hook-port": 5002 });
-            messager.once('hook::connected', function() {
-              messager.emit('test2', fixture);
-            });
-          },
-
-          "data should be valid buddy guy": function(data) {
-            assert.equal(data, fixture);
-          }
-        }
-      }
-    })
-  })
-}).export(module);
-*/
