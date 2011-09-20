ccd7d87b166b71d994bdd0f2f51e161ec4e487d1
Marak Squires <marak.squires@gmail.com>
2011-09-19 23:48:59 -0700

    [examples] Update Hook.emit example

--- examples/messaging/hook-emit.js
+++ examples/messaging/hook-emit.js
@@ -1,10 +1,9 @@
 /*
- * Creates a helloworld hook, then spawns three helloworld children
+ * Simple example for basic Hook.emit syntax
  */
 
 var Hook = require('../../lib/hookio').Hook;
 
-var pingPongModule = require('../../test/fixtures/pingPongModule.js');
 
 var hook1 = new Hook({ 
   name: "server-hook",
@@ -14,36 +13,19 @@ var hook2 = new Hook({
   name: "callback-hook",
 });
 
-
 hook1.on('*::hello', function(data, callback){
-  
-  //
-  // callback is the callback for this event,
-  // should it exist
-  //
-  var result = {
-    "text": "Why hello there!"
-  };
-  
-  hook1.emit(this.event + '::out', result);
-
+  console.log('calling result back ', data);
+  callback(null, data);
 })
 
 hook1.on('hook::ready', function(){
-  
   hook2.start();
-  
   hook2.on('hook::ready', function(){
-    
-    hook2.emit('hello');
-    
-    hook2.on('*::hello::out', function(data){
-      console.log('hello::out ', data);
+    hook2.on('hello::result', function(data){
+      console.log('result is back ', data);
     });
-    
-    
+    hook2.emit('hello', 'world');
   });
-  
 });
 
 hook1.start();
