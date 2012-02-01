2eee43ad43391ba9b9ad47b1c178157d82418ce7
Raj Kissu <rajkissu@gmail.com>
2012-02-01 12:22:34 +0800

    Modified logging test cases to take hook.log() and uncaught exceptions handling into account

--- test/logging/exception-handlers-test.js
+++ test/logging/exception-handlers-test.js
@@ -6,7 +6,7 @@
  */
 'use strict';
 
-var vows, fs, assert, winston, Hook;
+var vows, fs, assert, winston, Hook, hook, options;
 
 fs      = require('fs');
 vows    = require('vows');
@@ -14,38 +14,41 @@ assert  = require('assert');
 winston = require('winston');
 Hook    = require('../../lib/hookio').Hook;
 
-vows.describe('hook.io/logging/exception-handlers').addBatch({
-  "Given a configuration for logging exception handlers" : {
-    topic : function() {
-      return {
-        port    : 5002,
-        logger  : {
-          exceptionHandlers : {
-            file : { filename : __dirname + "/err.log" }
-          },
-
-          handleExceptions : true
-        }
-      };
+options = {
+  port    : 5002,
+  logger  : {
+    exceptionHandlers : {
+      file : { filename : __dirname + "/err.log" }
     },
 
-    "When a hook is initialized" : {
-      topic: function(options) {
-        var self, hook;
+    handleExceptions  : true,
+    exitOnError       : false
+  }
+};
+
+vows.describe('hook.io/logging/exception-handlers').addBatch({
+  "Given a hook with logging configuration" : {
+    topic : function() {
+      return new Hook(options);
+    },
+
+    "When a hook is ready" : {
+      topic: function(hook) {
+        var self;
 
         self  = this;
-        hook  = new Hook(options);
 
         hook.on('hook::ready', function() {
-          self.callback(null, this._winston, options.logger.exceptionHandlers);
+          self.callback(null, this._winston);
         });
 
         hook.start();
       },
 
-      "It should convert the object into Winston exception handlers" : function(err, hookWinston, types) {
-        var exceptionHandlers, name, capitalized;
+      "It should convert the object into Winston exception handlers" : function(err, hookWinston) {
+        var types, exceptionHandlers, name, capitalized;
 
+        types             = options.logger.exceptionHandlers;
         exceptionHandlers = hookWinston.exceptionHandlers;
 
         for (name in types) {
@@ -54,6 +57,28 @@ vows.describe('hook.io/logging/exception-handlers').addBatch({
 
           assert.instanceOf(exceptionHandlers[name], winston.transports[capitalized]);
         }
+      }
+    },
+
+    "When an uncaught exception is thrown" : {
+      topic: function(hook) {
+        var self = this;
+
+        // can't really throw an error as vows will catch it,
+        // so we simulate an uncaught exception
+        hook._winston._uncaughtException(new Error('This error should be logged'));
+
+        setTimeout(function() {
+          // we first need to wait for the handler to timeout
+          fs.readFile(hook.logger.exceptionHandlers.file.filename, 'utf-8', self.callback);
+        }, 4000);
+      },
+
+      "It should write to the specified log file" : function(err, data) {
+        assert.ifError(err);
+        assert.ok(/This error should be logged/.test(data));
+
+        fs.unlink(options.logger.exceptionHandlers.file.filename);
       }
     }
   }
